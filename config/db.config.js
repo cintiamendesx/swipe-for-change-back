31ba59cc1ecf9795f6e3ffdbb27da9027bea86ba
cintiamendesx <cintiamendesx@gmail.com>
2022-06-24 12:37:42 -0300

    url heroku server1

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 
-
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
 
 async function connect() {
   try {
